49e0fbe432ad8dd26c743c74680b5cb0ca4be5c1
lcl <2251086272@qq.com>
2025-08-11 10:16:03 +0800

    vue3 x项目初始化

---
+++ apps/vue3/src/views/file/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="file">
+    <el-button>Default</el-button>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+  .file {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background-color: #f0f0f0;
+  }
+</style>
