daf8922bee283ed831e260a8778586be3d446d49
lcl <2251086272@qq.com>
2025-08-19 23:20:27 +0800

    webpack4 - vue2, 添加新的html，并注入变量

--- apps/com_vue2/vue.config.js
+++ apps/com_vue2/vue.config.js
@@ -6,12 +6,12 @@
  * @LastEditTime: 2022-07-19 15:15:26
  * @Description: lcl
  */
-const path = require('path');
+const path = require("path");
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
 // 引入等比适配插件
-const px2rem = require('postcss-px2rem');
+const px2rem = require("postcss-px2rem");
 // 配置基本大小
 const postcss = px2rem({
   // 基准大小 baseSize，需要和rem.js中相同
@@ -21,22 +21,36 @@ module.exports = {
   devServer: {
     hot: true,
     proxy: {
-      '/api': {
+      "/api": {
         //本地服务接口地址
-        target: 'http://192.168.170.61:510',
+        target: "http://192.168.170.61:510",
         // target: 'http://192.168.175.230:80',
         ws: true,
         pathRewrite: {
-          '^/api': '/',
+          "^/api": "/",
         },
       },
     },
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.resolve.alias
-      .set('@', resolve('src'))
-      .set('assets', resolve('src/assets'))
-      .set('components', resolve('src/components'));
+      .set("@", resolve("src"))
+      .set("assets", resolve("src/assets"))
+      .set("components", resolve("src/components"));
+    // 新增 load.html 的处理配置
+    config.plugin("html-load").use(require("html-webpack-plugin"), [
+      {
+        template: "public/other/load.html", // load.html 的路径
+        filename: "/other/load.html", // 输出文件名
+        templateParameters: {
+          // 注入的全局变量
+          APP_NAME: "我的应用",
+          API_URL: "https://www.doubao.com/chat/17001215559706370",
+          REQUEST_URL: "http:/183.230.9.103:40000",
+        },
+        inject: false, // 不自动注入入口JS
+      },
+    ]);
   },
   lintOnSave: true,
   css: {
